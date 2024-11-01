e8249e34a2f09e59e19bedd5b5af0ad118fc0101
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2024-11-01 16:25:48 +0530

    new features

--- admin/src/addProject.jsx
+++ admin/src/addProject.jsx
@@ -4,9 +4,19 @@ import { useNavigate } from "react-router-dom";
 
 function AddProject(){
   
-   
+   const[Project,setProject]=useState(
+    {
+      projectName:"",
+      startDate:"",
+      targetDate:"",
+      siteLocation:"",
+      team:"",
+      details:"",
+      completedPercentage:""
+    })
+  
+  
 
-  const [ImagePreviewUrl,setImagePreviewUrl]=useState(null)
    
   const [employeesData,setemployeesData]=useState([])
     useEffect(()=>{
@@ -16,28 +26,49 @@ function AddProject(){
         }
        }).then((response)=>{
        response.json().then((data)=>{
-        setemployeesData(data)
+        setemployeesData(data)      
        })
         })
+        
     },[])
 
     const [Team,setTeam]=useState([])
-    let temp=""
-
+     
+   const [temp,settemp]=useState("")
     return <div style={{padding:100,background:"#B5B5B5"}}>
         <Card elevation={24}style={{height:"auto"}}>
            <div style={{display:"flex",justifyContent:"center"}}> <h3> Project Details</h3> </div>
          <div style={{display:"flex", justifyContent:"space-evenly"}}>
                <div style={{padding:50}}>
-                   <TextField label="ProjectName" variant="standard" /><br></br>
+                   <TextField label="ProjectName" variant="standard" onChange={(event)=>{
+                    setProject((previousData)=>({
+                      ...previousData,
+                      projectName:event.target.value
+                    }))
+                   }}/><br></br>
                    <div style={{display:"flex",gap:30}}>
-                      <TextField label="Start Date" variant="standard"/>
-                      <TextField label="Target Date" variant="standard"/><br></br>
+                      <TextField label="Start Date" variant="standard"  onChange={(event)=>{
+                     setProject((previousData)=>({
+                      ...previousData,
+                      startDate:event.target.value
+                    }))
+                   }}/>
+                      <TextField label="Target Date" variant="standard"  onChange={(event)=>{
+                    setProject((previousData)=>({
+                      ...previousData,
+                      targetDate:event.target.value
+                    }))
+                   }}/><br></br>
                    </div>
-                   <TextField label="Site Location" variant="standard"/><br></br>
+                   <TextField label="Site Location" variant="standard"   onChange={(event)=>{
+                    setProject((previousData)=>({
+                      ...previousData,
+                      siteLocation:event.target.value
+                    }))
+                   }}/><br></br>
                    <br></br>
                    Select Team Members: <select id="team" name="team" onChange={()=>{
-                     temp=document.getElementById("team").value
+                     settemp(document.getElementById("team").value)
                    }} >
                       <option value=""disabled selected hidden>Team</option>
                       { employeesData.map(e=>{
@@ -50,7 +81,7 @@ function AddProject(){
                           if(Team.find((e)=>e===temp)){
                             alert("Team Member Already Added")
                           }else{
-                            setTeam(existingValue=>[...existingValue,temp])
+                            setTeam((existingValue)=>[...existingValue,temp])                          
                           }                         
                         }}>Add</Button>
                         <Button variant="contained" style={{background:"grey"}} onClick={()=>{
@@ -59,38 +90,80 @@ function AddProject(){
                             return(newValue)
                           })
                          }}>delete</Button>
+                         
                       </div>
                      <Card elevation={24} style={{height:200,width:200,padding:10,overflow:"auto",background:"#A9A9A9"}}>
                       <h3><u>Team Members</u></h3>
                      <div>
                        {Team.map(e=>{
                         return <h5>{e}</h5>
-                       })}
+                       })
+                       }                        
                      </div>
-                     </Card>
+                     </Card><br />
+                      <Button variant="contained" style={{background:"grey"}} onClick={()=>{
+                            setProject((previousData)=>({
+                           ...previousData,
+                           team:Team
+                           }))            
+                           }}>Upload Members </Button>
                      <br></br>
                      <h3>Details</h3>
                      <textarea style={{height:300,width:500}}></textarea>
+                     <div>
+                        <h3>Completed Percentage</h3>
+                        <select name="" id="completePercentage" onChange={()=>{
+                          const value=document.getElementById("completePercentage").value
+                          console.log(value);                         
+                        }}>
+                          <option value="0%">0%</option>
+                          <option value="10%">10%</option>
+                          <option value="20%">20%</option>
+                          <option value="30%">30%</option>
+                          <option value="40%">40%</option>
+                          <option value="50%">50%</option>
+                          <option value="60%">60%</option>
+                          <option value="70%">70%</option>
+                          <option value="80%">80%</option>
+                          <option value="90%">90%</option>
+                          <option value="100%">100%</option>
+                        </select>
+                     </div>
                </div>
-               <div style={{height:400,width:400}}>
+               <div style={{height:"auto",width:400}}>
                 <Card elevation={24}style={{height:"auto",width:"auto"}}>
-                    <h3>Upload Project Title Image</h3> <br></br>
+                    <h3>Upload Project Title Image</h3> 
                     <input type="file" accept="image/*" onChange={(event)=>{
                            const file = event.target.files[0];
                   
                     }}></input>
+                    <Button variant="contained" style={{background:"grey"}}>upload</Button>
                 </Card>
                 <br />
-                <Card elevation={24}style={{height:"auto",width:"auto"}}>
+                <Card elevation={24}style={{height:250,width:"auto"}}>
                     <h3>Upload Project Photos</h3> <br></br>
                     <input type="file" multiple accept="image/*" onChange={(event)=>{
                            const file = event.target.files[0];                               
                     }}></input>
+                    <Button variant="contained" style={{background:"grey"}}>upload</Button>
+                </Card>
+                <br />
+                <Card elevation={24}style={{height:250,width:"auto"}}>
+                    <h3>Upload Project Files</h3> <br></br>
+                    <input type="file" multiple accept="image/*" onChange={(event)=>{
+                           const file = event.target.files[0];                               
+                    }}></input>
+                    <Button variant="contained" style={{background:"grey"}}>upload</Button>
                 </Card>
                </div>
            </div>
            <div style={{display:"flex",justifyContent:"center", marginBottom:50}}>
-             <Button variant="contained" style={{background:"grey"}}>Add Project</Button> 
+             <Button variant="contained" style={{background:"grey"}} onClick={()=>{
+
+             }}>Add Project</Button> 
+              <Button variant="contained" style={{background:"grey"}} onClick={()=>{
+              console.log(Project)           
+             }}>Project</Button> 
            </div>
         </Card>
     </div>
