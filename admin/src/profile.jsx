bed737cb99cfc552673dee445823f50e9e52b6ea
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2024-11-23 09:14:12 +0530

    profile page update

--- admin/src/profile.jsx
+++ admin/src/profile.jsx
@@ -6,8 +6,11 @@ import { Card } from "@mui/material"
 function Profile(){
 
 
-    return <div>
-        <h1>profile</h1>
+    return <div style={{background:"grey",width:"100vw",height:"100vh",display:"flex",justifyContent:"center",paddingTop:100}}>
+        <Card elevation={14} style={{height:500,width:300}}>
+        <h2 style={{display:"flex",justifyContent:"center"}}>Profile</h2>
+       
+      </Card>
     </div>
 }
 export default Profile
