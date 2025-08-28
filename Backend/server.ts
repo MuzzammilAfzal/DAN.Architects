98a346b39dfe48ac1b813a3987fa526d8b26d617
Muzzammil Afzal <muzzu2605afzall@gmail.com>
2025-08-29 00:05:14 +0530

    cors

--- Backend/server.ts
+++ Backend/server.ts
@@ -11,7 +11,7 @@ require("dotenv").config();
 import { z} from "zod"
 const app = express()
 const port = 5000
- app.use(cors(app.use(cors({origin:"https://dan-architects-1.onrender.com",methods:["GET","POST","PUT","DELETE"],credentials:true}))))
+ app.use(cors(app.use(cors({origin:"https://dan-architects-space.vercel.app/admin",methods:["GET","POST","PUT","DELETE"],credentials:true}))))
  app.use(express.json())
  const secertKey="DAN.Architects"
 
